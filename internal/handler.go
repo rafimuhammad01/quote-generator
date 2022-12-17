fa90ac681f51600eb834c0773c9c927cac66ee32
Mario Serano <marioserano55@gmail.com>
2022-12-18 00:54:29 +0700

    feat: add shuffle quotes functionality

--- internal/handler.go
+++ internal/handler.go
@@ -3,12 +3,14 @@ package internal
 import (
 	"context"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type Service interface {
 	GenerateQuote(ctx context.Context, input GenerateQuote) (*Quote, error)
+	ShuffleQuote(ctx context.Context, input ShuffleQuote) (*Quote, error)
 }
 
 type Handler struct {
@@ -27,6 +29,24 @@ func (h *Handler) GenerateQuote(c *fiber.Ctx) error {
 	}
 
 	resp, err := h.service.GenerateQuote(c.Context(), reqParams)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(http.StatusOK).JSON(JSONResponse{
+		Message: "OK",
+		Data:    resp,
+	})
+}
+
+func (h *Handler) ShuffleQuote(c *fiber.Ctx) error {
+	var reqParams ShuffleQuote
+	err := c.QueryParser(&reqParams)
+	if err != nil {
+		return GenerateError(ErrInternalServer, fmt.Sprintf("error when parse query params: %s", err.Error()))
+	}
+
+	resp, err := h.service.ShuffleQuote((c.Context()), reqParams)
 	if err != nil {
 		return err
 	}
